ebe7f9de738db2685f6e4ff5a900c8ce035668b6
Rohit <rohit.jagtap@wurth-it.in>
2019-02-26 18:43:37 +0530

    first commit

---
+++ TaskManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDDWeb.Models
+{
+    public class TaskManager
+    {
+        public bool AddTask(string task)
+        {
+            if (string.IsNullOrEmpty(task))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+    }
+}
---
+++ TaskManagerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDWeb.Models;
+namespace TDDWeb.Tests
+{
+    [TestClass]
+    public class TaskManagerTests
+    {
+        [TestMethod]
+        public void AddTask()
+        {
+            TaskManager task = new TaskManager();
+            bool result = task.AddTask("Test");
+            Assert.IsNotNull(result);
+        }
+    }
+}
