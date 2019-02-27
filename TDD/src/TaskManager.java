07583c7adb7f83360fe74769e25a913aa93f6dae
wn00103413 <bhavesh.jain@wurth-it.in>
2019-02-27 10:58:44 +0530

    User can get count of listitems
    User can get task from list by taskid
    
    User can delete a task by id

--- TDD/src/TaskManager.java
+++ TDD/src/TaskManager.java
@@ -3,9 +3,52 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaskManager
 {
-	List<String> taskList;
 	private static AtomicInteger ID_GENERATOR = new AtomicInteger(1000);
-	
+	public class Task 
+	{
+		int id;
+		String query;
+		boolean isActive;
+		List<Task> taskList;
+
+		public int saveTask(String query)
+		{
+			Task task = new Task();
+			task.id = ID_GENERATOR.getAndIncrement();
+			task.query = query;
+			task.isActive = true;
+			taskList.add(task);
+			return task.id;
+		}
+		
+		public Task getTaskById(int id)
+		{
+			for (Task task : taskList) {
+				if((task.id == id) && task.isActive)
+					return task;
+			}
+			return null;
+		}
+
+		public int getTaskCount() {
+			int count = 0;
+			for (Task task : taskList) {
+				if(task.isActive)
+					count++;
+			}
+			return count;
+		}
+
+		public boolean deleteTaskById(int id) {
+			Task task = new Task();
+			if(!(taskList.isEmpty()) || (taskList.size() > 0)) {
+				task = getTaskById(id);
+				task.isActive = false;
+			}
+			return false;
+		}
+	}
+
 	public boolean AddTask (String query)
 	{
 		if((query == "") || (query == null))
@@ -14,10 +57,25 @@ public class TaskManager
 	}
 	
 	public int SaveTaskId(String query) {
-		if(this.AddTask(query)) {
-			taskList.add(query);
-			return ID_GENERATOR.getAndIncrement();
-		}
+		Task task = new Task();
+		if(this.AddTask(query))
+			return task.saveTask(query);
 		return -1;
+	}
+
+	public Task getTaskByTaskId(int taskId) {
+		Task task = new Task();
+		return task.getTaskById(taskId);
+	}
+	
+
+	public int getTaskCount() {
+		Task task = new Task();
+		return task.getTaskCount();
+	}
+
+	public boolean deleteTaskById(int id) {
+		Task task = new Task();
+		return task.deleteTaskById(id);
 	}
 }
