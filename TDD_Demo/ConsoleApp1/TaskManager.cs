7f48010c94a432714c5d2703fd83fe8c7dfc84c4
47808192 <shivangi.thakur@wurth-it.in>
2019-02-27 10:00:40 +0530

    First Commit

---
+++ TDD_Demo/ConsoleApp1/TaskManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class TaskManager
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+      
+            TaskManager S1 = new TaskManager();
+            bool b1 = S1.AddTask("");
+
+            Console.WriteLine(b1);
+        }
+        Boolean AddTask(String s)
+        {
+            // Console.WriteLine("Hi");
+            if (String.IsNullOrEmpty(s))
+            {
+                Console.WriteLine("string is empty");
+                Console.Read();
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("string is not empty");
+                Console.Read();
+                return true;
+            }
+
+        }
+        
+    }
+}
+/*public class TaskManager
+{
+   public Boolean AddTask(String s)
+    {
+        if (String.IsNullOrEmpty(s))
+        { return false; }
+        else
+        {
+            Console.WriteLine("string is not empty");
+            return true;
+        }
+            
+    }
+}*/
