49063ac9fc2ac0b0ed4913b4014ff55807e65aea
Sagar Chaure <WN00103415@witin.local>
2019-02-27 10:59:21 +0530

    Added 4 requirements

--- Test.js
+++ Test.js
@@ -25,4 +25,45 @@ describe('TDD', function() {
         }
     }
 
+    var aTaskID = [];
+    var iTaskValue = 0;
+    class Task {
+        constructor(TaskID) {
+            this.TaskID = TaskID;
+        }
+        fnSaveTaskID(iTaskID) {
+            aTaskID.push(iTaskID)
+            if (aTaskID.includes(iTaskID)) {
+                iTaskValue = iTaskValue + 1
+                return iTaskValue;
+            } else {
+                return -1
+            }
+
+        }
+
+        fnReturnTask(iTId) {
+            return aTaskID[iTId - 1]
+        }
+
+        fnReturnCountValue() {
+            return aTaskID.length
+        }
+
+        fnDeleteTask(iTaskDelete) {
+            aTaskID.splice((iTaskDelete - 1), 1);
+
+        }
+
+    }
+
+    var task = new Task();
+    // var task = new Task('test');
+    // var task = new Task('test');
+    console.log(task.fnSaveTaskID('a'));
+    console.log(task.fnSaveTaskID('b'));
+    console.log(task.fnSaveTaskID('c'));
+    task.fnDeleteTask(2);
+    console.log(aTaskID);
+    console.log(task.fnReturnCountValue())
 });
