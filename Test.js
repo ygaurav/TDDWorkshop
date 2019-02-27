a3077619955edc09fceb670e6fb293036b401ca4
Sagar Chaure <WN00103415@witin.local>
2019-02-27 12:06:28 +0530

    Added 5th requirement

--- Test.js
+++ Test.js
@@ -14,7 +14,20 @@ describe('TDD', function() {
         taskManager.AddTask
     });
 
+    it('It should return Task ID on Success', function() {
+        var task = new Task();
+        task.fnSaveTaskID('a');
+    });
 
+    it('It should return Task Based on Task ID', function() {
+        var task = new Task();
+        //task.fnReturnTask(8);
+    });
+
+    it('It should return Nummber of Tasks', function() {
+        var task = new Task();
+        task.fnReturnCountValue;
+    })
     class TaskManager {
         constructor(sName = '') {
             this.sName = sName;
@@ -32,13 +45,24 @@ describe('TDD', function() {
             this.TaskID = TaskID;
         }
         fnSaveTaskID(iTaskID) {
-            aTaskID.push(iTaskID)
-            if (aTaskID.includes(iTaskID)) {
-                iTaskValue = iTaskValue + 1
-                return iTaskValue;
-            } else {
-                return -1
+            var sTaskString = [];
+            sTaskString = iTaskID.split(' ');
+            for (var i = 0; i < sTaskString.length; i++) {
+                if (sTaskString[i].includes('@')) {
+                    var pattern = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
+                    var sdateValue = sTaskString[i].split('@')[1]
+                    if (pattern.test(sdateValue)) {
+                        aTaskID.push(iTaskID);
+                        iTaskValue = iTaskValue + 1
+                        return iTaskValue;
+                    } else {
+                        return -1
+                    }
+                } else {
+                    return -1
+                }
             }
+
 
         }
 
@@ -60,7 +84,7 @@ describe('TDD', function() {
     var task = new Task();
     // var task = new Task('test');
     // var task = new Task('test');
-    console.log(task.fnSaveTaskID('a'));
+    console.log(task.fnSaveTaskID('going on @28/02/1967'));
     console.log(task.fnSaveTaskID('b'));
     console.log(task.fnSaveTaskID('c'));
     task.fnDeleteTask(2);
