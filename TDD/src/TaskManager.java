113f5cd8660c6b2cc6c10e8e8aea677740435b58
wn00103413 <bhavesh.jain@wurth-it.in>
2019-03-04 13:37:48 +0530

    Requirements 5 and 6 with testcases

--- TDD/src/TaskManager.java
+++ TDD/src/TaskManager.java
@@ -10,74 +10,94 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TaskManager
 {
 	private static AtomicInteger ID_GENERATOR = new AtomicInteger(1000);
+	List<Task> taskList = new ArrayList<TaskManager.Task>();
+	
 	public class Task 
 	{
 		int id;
 		String query;
 		boolean isActive;
 		Date deadLine;
-		List<Task> taskList;
 
 		public int saveTask(String query) throws ParseException
 		{
 			String[] queryStrings;
 			Task task = new Task();
-			int countString = 0;
 			task.id = ID_GENERATOR.getAndIncrement();
-			queryStrings = query.split(" ");
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Calendar calendar = new GregorianCalendar();
 			Date date = new Date();
-			for (String string : queryStrings) {
+			queryStrings = query.split(" ");
+			task.query = queryStrings[0];
+			for(String string : queryStrings) {
+				Calendar calendar = new GregorianCalendar();
+				boolean flag = true;
 				if(string.contains("@")) {
-					countString++;
-				}
-			}
-			for (String string : queryStrings) {
-				if(string.contains("@")) {
-					countString++;
-				}
-				if(countString != 1) {
-					countString--;
-					break;
-				} else {
-					if(string.contains("@")) {
-						if(string.length() == 1) {
-							calendar.add(Calendar.DAY_OF_MONTH, 7);
-							try {
-								date = sdf.parse(calendar.getTime()+"");
-							} catch (Exception e) {
-								e.printStackTrace();
+					if(string.length() == 1) {
+						calendar.add(Calendar.DAY_OF_MONTH, 7);
+						try {
+							date = calendar.getTime();
+							task.deadLine = date;
+							flag = false;
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					} else if(Character.isDigit(string.charAt(1))) {
+					    string = string.substring(1);
+						String[] fullMonths = {"1", "3", "5", "7", "8", "10", "12"};
+						string = string.substring(1);
+						if(string.length() > 1) {
+					    	String[] splitDate = string.split("/");
+							if(Integer.parseInt(splitDate[1]) > 13) {
+								flag = false;
+							} else {
+								for (String month : fullMonths) {
+									if(splitDate[1].equals(month)) {
+										if(Integer.parseInt(splitDate[0]) > 31) {
+											flag = false;
+										}
+									}
+									else if(splitDate[1].equals("2")) {
+										if(Integer.parseInt(splitDate[0]) < 29) {
+											if ((Integer.parseInt(splitDate[2]) % 4 != 0) && (Integer.parseInt(splitDate[0]) == 29)) {
+												flag = false;
+											} else {
+												flag = true;
+											}
+										} else {
+											flag = false;
+										}
+									} else {
+										if(Integer.parseInt(splitDate[0]) > 30) {
+											flag = false;
+										}
+									}
+								}
+								if(flag) {
+									try {
+										date = sdf.parse(string);
+										if((sdf.format(date).equals(sdf.format(new Date()))) || (date.after(new Date()))) {
+											task.deadLine = date;
+										}
+										else {
+											calendar.add(Calendar.DAY_OF_MONTH, 7);
+											try {
+												date = calendar.getTime();
+												task.deadLine = date;
+											} catch (Exception e) {
+												e.printStackTrace();
+											};
+										}
+									} catch (Exception e) {
+										e.printStackTrace();
+									}
+								}
 							}
 						}
-						else if(Character.isDigit(string.charAt(1))) {
-							if(string.length() > 1) {
-								try {
-									date = sdf.parse(string);
-								} catch (Exception e) {
-									e.printStackTrace();
-								}
-							} else if(string.length() == 1) {
-								calendar.add(Calendar.DAY_OF_MONTH, 7);
-								try {
-									date = sdf.parse(calendar.getTime()+"");
-								} catch (Exception e) {
-								}
-							}
-						}
-						if((sdf.format(date).equals(sdf.format(new Date()))) || (date.after(new Date())))
-							deadLine = date;
 					}
 				}
 			}
-			
-			
-			
-			
-			
-			if(deadLine != null)
+			if(task.deadLine != null)
 			{
-				task.query = query;
 				task.isActive = true;
 				taskList.add(task);
 				return task.id;
@@ -111,6 +131,15 @@ public class TaskManager
 			}
 			return false;
 		}
+
+		public List<Task> getDeadLinePassedTasks() {
+			List<Task> taskDatePassedList = new ArrayList<>();
+			for (Task task : taskList) {
+				if((task.isActive) && (task.deadLine.before(new Date())))
+					taskDatePassedList.add(task);
+			}
+			return taskDatePassedList;
+		}
 	}
 
 	public boolean AddTask (String query) {
@@ -122,15 +151,20 @@ public class TaskManager
 	public int SaveTaskId(String query) throws ParseException {
 		Task task = new Task();
 		if(this.AddTask(query))
-			if(task.saveTask(query) != -1) {
-				return 1;
-			}
+			return task.saveTask(query);
 		return -1;
 	}
 
 	public Task getTaskByTaskId(int taskId) {
 		Task task = new Task();
 		return task.getTaskById(taskId);
+	}
+
+	public boolean getTaskByTaskIdFlag(int taskId) {
+		Task task = new Task();
+		if(task.getTaskById(taskId) != null)
+			return true;
+		return false;
 	}
 
 	public int getTaskCount() {
@@ -141,5 +175,10 @@ public class TaskManager
 	public boolean deleteTaskById(int id) {
 		Task task = new Task();
 		return task.deleteTaskById(id);
+	}
+	
+	public List<Task> getDeadLinePassedTasks() {
+		Task task = new Task();
+		return task.getDeadLinePassedTasks();
 	}
 }
--- TDD/src/TaskManagerTest.java
+++ TDD/src/TaskManagerTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import java.text.ParseException;
+import java.util.List;
 
 public class TaskManagerTest {
 	@Test
@@ -24,16 +26,77 @@ public class TaskManagerTest {
 	}
 	
 	@Test
-	public void testTaskManagerSaveTaskFailed() {
+	public void testSaveTaskFailed() throws ParseException {
 		TaskManager task = new TaskManager();
 		int val = task.SaveTaskId("");
 		assertEquals(-1, val);
 	}
 	
 	@Test
-	public void testTaskManagerSaveTaskPassed() {
+	public void testSaveTaskFailedNoDateSrtring() throws ParseException {
 		TaskManager task = new TaskManager();
-		int val = task.SaveTaskId("Bhavesh");
+		int val = task.SaveTaskId("bhavesh");
 		assertEquals(-1, val);
+	}
+	
+	@Test
+	public void testSaveTaskDateFieldWithAlpha() throws ParseException {
+		TaskManager task = new TaskManager();
+		int val = task.SaveTaskId("bhavesh @abc");
+		assertEquals(-1, val);
+	}
+	
+	@Test
+	public void testSaveTaskFailedWrongDate() throws ParseException {
+		TaskManager task = new TaskManager();
+		int val = task.SaveTaskId("bhavesh @22/22/2019");
+		assertEquals(-1, val);
+	}
+	
+	@Test
+	public void testSaveTaskPassedNoDate() throws ParseException {
+		TaskManager task = new TaskManager();
+		int val = task.SaveTaskId("bhavesh @02/05/2019");
+		boolean testVal = task.getTaskByTaskIdFlag(val);
+		assertEquals(testVal, true);
+	}
+	
+	@Test
+	public void testSaveTaskPassedRigthDate() throws ParseException {
+		TaskManager task = new TaskManager();
+		int val = task.SaveTaskId("bhavesh @02/02/2019");
+		boolean testVal = task.getTaskByTaskIdFlag(val);
+		assertEquals(testVal, true);
+	}
+	
+	@Test
+	public void testSaveTaskPassedMulitpleDate() throws ParseException {
+		TaskManager task = new TaskManager();
+		int val = task.SaveTaskId("bhavesh @02/02/2019 @ xyz @22/22/2019");
+		boolean testVal = task.getTaskByTaskIdFlag(val);
+		assertEquals(testVal, true);
+	}
+	
+	@Test
+	public void testSaveTaskPassedMulitpleWrongDates() throws ParseException {
+		TaskManager task = new TaskManager();
+		int val = task.SaveTaskId("bhavesh @xyz @22/22/2019");
+		boolean testVal = task.getTaskByTaskIdFlag(val);
+		assertEquals(testVal, false);
+	}
+	
+	@Test
+	public void testSaveTaskPassedMulitpleWrongRightDate() throws ParseException {
+		TaskManager task = new TaskManager();
+		int val = task.SaveTaskId("bhavesh @02/02/2019 @xyz @22/22/2019");
+		boolean testVal = task.getTaskByTaskIdFlag(val);
+		assertEquals(testVal, true);
+	}
+	
+	@Test
+	public void testGetDeadLinePassedTasks() {
+		TaskManager task = new TaskManager();
+		List<TaskManager.Task> val = task.getDeadLinePassedTasks();
+		assertEquals(val.size(), 0);
 	}
 }
